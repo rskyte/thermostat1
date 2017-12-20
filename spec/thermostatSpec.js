fff7e490b0dcf7eb7f1c12eba9d0d38db3e05290
tom <tomallpress@hotmail.com>
2017-12-20 14:23:44 +0000

    added turnUp function to increase the thermostats temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,4 +8,9 @@ describe("Thermostat", function() {
   it("should default to 20 degrees", function() {
     expect(thermostat.temp).toEqual(20)
   })
+
+  it("should increase temp by desired amount when called upon", function() {
+    thermostat.turnUp(5);
+    expect(thermostat.temp).toEqual(25);
+  });
 })
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,3 +3,7 @@ DEFAULT_TEMPERATURE = 20
 function Thermostat() {
   this.temp = DEFAULT_TEMPERATURE
 }
+
+Thermostat.prototype.turnUp = function(x) {
+  this.temp += x;
+}
